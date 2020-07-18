54897d2582dcd30d5d27f8c2e20c14bd06738c05
pmohun <philmohun@gmail.com>
2020-07-18 13:18:35 -0400

    added command line support

--- calvin/calvin.py
+++ calvin/calvin.py
@@ -1,8 +1,19 @@
-import openai, requests, os, json
+import openai
+import requests
+import os
+import json
+import argparse
+import sys
+import tqdm
+
+
+def load_json():
+  f = open('../prompts.json')
+  data = json.load(f)
+  return data
 
 def prompt_list():
-  f = open('prompts.json')
-  data = json.load(f)
+  data = load_json()
   print(list(data.keys()))
   return data
 
@@ -11,9 +22,56 @@ def create(prompt, data):
   response = openai.Completion.create(engine="davinci", prompt=data[prompt][0]['prompt'], max_tokens=150)
   output = response["choices"][0]['text']
 
+stop = "\n-"
+
+def main(prompt):
+    data = load_json()
+    parser = argparse.ArgumentParser(description=None)
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        action="count",
+        dest="verbosity",
+        default=0,
+        help="Set verbosity.",
+    )
+    parser.add_argument("-n", "--number", type=int, default=10)
+    parser.add_argument("-e", "--engine", default="davinci")
+    parser.add_argument("-b", "--best_of", type=int)
+    parser.add_argument("-m", "--max_batch", type=int, default=25)
+    args = parser.parse_args()
+
+    n = args.number
+    batches = []
+    while n > 0:
+        take = min(n, args.max_batch)
+        batches.append(take)
+        n -= take
+
+    choices = []
+    for b in tqdm.tqdm(batches):
+        completion = openai.Completion.create(
+            prompt=prompt,
+            n=b,
+            engine=args.engine,
+            max_tokens=300,
+            stop=stop,
+            temperature=data[prompt][0]['temperature'],
+            logprobs=0,
+        )
+        choices += completion.choices
+
+    def score(a):
+        return sum(a.logprobs.token_logprobs) / len(a.logprobs.token_logprobs)
+
+    choices = sorted(choices, key=lambda a: -score(a))
+    for i, choice in enumerate(choices):
+        print(f"======")
+        print(choice.text.strip())
+
+    return 0
+
+
 if __name__ == "__main__":
-  data = prompt_list
-  print(type(data))
-  print('Please input a prompt: ')
-  prompt = input()
-  create(prompt, data)
+    prompt = input('Enter prompt: ')
+    sys.exit(main(prompt))
