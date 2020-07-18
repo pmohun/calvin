72e910e409c1166f700bbdd19dc0e2299dea4303
pmohun <philmohun@gmail.com>
2020-07-18 02:56:10 -0400

    updated setup file to include description

--- setup.py
+++ setup.py
@@ -25,3 +25,34 @@ setup(
     author_email="calvin@philmohun.com",
     url="https://github.com/pmohun/calvin",
 )
+
+DESCRIPTION = "Simple premade prompts for interacting with GPT-3"
+LONG_DESCRIPTION = """
+**calvin** is a Python package that provides premade prompts for interacting with language models like GPT-3. It simplifies the learning curve by providing ease to use examples and sample text to get started. Additionally, it has the broader goal
+of becoming **the most comprehensive repository of prompts for interacting with language models.**
+"""
+
+DISTNAME = "calvin"
+LICENSE = "MIT"
+AUTHOR = "philmohun"
+EMAIL = "calvin@philmohun.com"
+URL = ""
+DOWNLOAD_URL = ""
+PROJECT_URLS = {
+    "Bug Tracker": "https://github.com/pmohun/calvin/issues",
+    "Documentation": "https://github.com/pmohun/calvin
+    "Source Code": "https://github.com/pmohun/calvin",
+}
+CLASSIFIERS = [
+    "Development Status :: 2 - Pre-Alpha",
+    "Environment :: Console",
+    "Operating System :: OS Independent",
+    "Intended Audience :: Science/Research",
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.6",
+    "Programming Language :: Python :: 3.7",
+    "Programming Language :: Python :: 3.8",
+    "Topic :: Scientific/Engineering",
+    "Topic :: Communications :: Chat",
+]
