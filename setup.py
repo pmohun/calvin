91b26d858f5f0e125a2da142bc80e3ec1a51fcca
pmohun <philmohun@gmail.com>
2020-07-20 01:01:47 -0400

    Updated setup.py file to inclide BSD-3 license and standard boilerplate text

--- setup.py
+++ setup.py
@@ -1,58 +1,22 @@
-import os
+import setuptools
 
-from setuptools import find_packages, setup
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
-version_contents = {}
-with open(
-    os.path.join(os.path.abspath(os.path.dirname(__file__)), "calvin/version.py")
-) as f:
-    exec(f.read(), version_contents)
-
-setup(
+setuptools.setup(
     name="calvin",
-    description="Python library for interacting with the OpenAI api",
-    version=version_contents["VERSION"],
-    install_requires=[
-        'requests >= 2.20; python_version >= "3.0"',
-        'requests[security] >= 2.20; python_version < "3.0"',
-    ],
-    extras_require={},
-    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*",
-    scripts=[],
-    # packages=find_packages(exclude=["tests", "tests.*"]),
-    # package_data={"openai": ["data/ca-certificates.crt"]},
-    author="Philip Mohun",
+    version="0.0.1",
+    author="Phil Mohun, Malcolm Navarro, DNE LLC",
     author_email="calvin@philmohun.com",
+    description="calvin is a Python library for developers using GPT-3.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url="https://github.com/pmohun/calvin",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD-3 License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
 )
-
-DESCRIPTION = "Simple premade prompts for interacting with GPT-3"
-LONG_DESCRIPTION = """
-**calvin** is a Python package that provides premade prompts for interacting with language models like GPT-3. It simplifies the learning curve by providing ease to use examples and sample text to get started. Additionally, it has the broader goal
-of becoming **the most comprehensive repository of prompts for interacting with language models.**
-"""
-
-DISTNAME = "calvin"
-LICENSE = "MIT"
-AUTHOR = "philmohun"
-EMAIL = "calvin@philmohun.com"
-URL = ""
-DOWNLOAD_URL = ""
-PROJECT_URLS = {
-    "Bug Tracker": "https://github.com/pmohun/calvin/issues",
-    "Documentation": "https://github.com/pmohun/calvin
-    "Source Code": "https://github.com/pmohun/calvin",
-}
-CLASSIFIERS = [
-    "Development Status :: 2 - Pre-Alpha",
-    "Environment :: Console",
-    "Operating System :: OS Independent",
-    "Intended Audience :: Science/Research",
-    "Programming Language :: Python",
-    "Programming Language :: Python :: 3",
-    "Programming Language :: Python :: 3.6",
-    "Programming Language :: Python :: 3.7",
-    "Programming Language :: Python :: 3.8",
-    "Topic :: Scientific/Engineering",
-    "Topic :: Communications :: Chat",
-]
